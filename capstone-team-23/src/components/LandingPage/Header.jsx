113e87f586729baea6fa5f6191481fe499fab636
parthppc <contracp@sheridancollege.ca>
2022-02-17 21:57:19 -0500

    Header Added

---
+++ capstone-team-23/src/components/LandingPage/Header.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Header() {
+    return <div>
+    
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container-fluid">
+           <a class="navbar-brand" href="#">FixeItNow</a>
+        </div>
+    </nav>
+    
+    </div>
+}
+
+export default Header;
