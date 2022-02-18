a9a5a28c95bbc15c36e1b2a1af79d54f9950e9da
parthppc <contracp@sheridancollege.ca>
2022-02-17 21:58:27 -0500

    First commit to develop

--- capstone-team-23/src/components/LandingPage/Header.jsx
+++ capstone-team-23/src/components/LandingPage/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
         <div class="container-fluid">
            <a class="navbar-brand" href="#">FixeItNow</a>
+           <a class="navbar-brand" href="#">Login</a>
         </div>
     </nav>
     
