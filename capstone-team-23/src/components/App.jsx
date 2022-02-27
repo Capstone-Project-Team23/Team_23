c5b94f3fea1d6dd08bd272e37d783aa285a40087
ConfidentProgrammer <deepp2618@gmail.com>
2022-02-27 11:33:41 -0500

    Merged

--- capstone-team-23/src/components/App.jsx
+++ capstone-team-23/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import Homepage from './Home Page/WholePage/HomePage.jsx'
-
+import SignUpPage from './Sign Up Page/WholePage/SignUpPage.jsx'
 function App() {
   return (
     <><div class = "">
        
         <Homepage />
+        <SignUpPage />
       </div></>
   )
 }
