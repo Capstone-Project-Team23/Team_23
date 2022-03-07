58f8312b20ca53c5259383a901e0771062a93bd9
parthppc <contracp@sheridancollege.ca>
2022-03-07 18:59:41 -0500

    contactus form backend added but  not working

--- capstone-team-23/src/components/ContactUs/ContactUsForm/ContactForm.jsx
+++ capstone-team-23/src/components/ContactUs/ContactUsForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React, { useState } from "react";
 import "./form.css"
 import InputField from "../../Reusable Components/InputFields/InputField";
 import Label from "../../Reusable Components/Label/Label";
@@ -8,26 +8,50 @@ import Button from "../../Reusable Components/Buttons/Button";
 import Header from "../../Reusable Components/Header/Header";
 import {style, styleHeader, labelStyle, Hed, textAreaStyle, textAreaStyle_Mobile} from './formstyle'
 function ContactForm() {  
+
+
+  //Contact form back end
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:3000/contactus", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+  /////backend End
+
     const html = [
         <Header text={Hed} style={styleHeader} />,
         <br></br>,
-        <Label style={window.innerWidth<480 ? {marginLeft: "1.5rem"} :labelStyle} name="Full Name" />,
-        <InputField placeholder="John Cena" width="250px" />,
-        <br></br>,
-        <Label style={window.innerWidth<480 ? {marginLeft: "1.5rem"} :labelStyle} name="Email" />,
-        <InputField placeholder="John@gmail.com" width="250px" />,
-        <br></br>,
-        <Label style={window.innerWidth<480 ? {marginLeft: "1.5rem"} :labelStyle} name="Number" />,
-        <InputField placeholder="###-###-####" width="250px" />,
-        <br></br>,
-        <Label style={window.innerWidth<480 ? {marginLeft: "1.5rem"} :labelStyle} name="Message" />,
-         <textarea style={window.innerWidth<480? textAreaStyle_Mobile :textAreaStyle}></textarea>,
-         <br></br>,
-        <Button buttonText="Send" width="250px" style={
-          {
-            width:"16rem",
-            marginTop:"1rem"
-          }} />,
+        <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" required />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" required />
+            </div>
+            <div>
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" required />
+            </div>
+            <button type="submit">{status}</button>
+        </form>,
       ];
     return <div >
       <ShadowRect style={style} html={html} />
