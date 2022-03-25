ed455954c77bf1b292e17f412d9a9395260c90c5
parthppc <contracp@sheridancollege.ca>
2022-03-24 22:11:56 -0400

    Revert "Merge branch 'CustomerDashBoard' of https://github.com/Capstone-Project-Team23/Team_23 into CustomerDashBoard"
    This reverts commit d626845b58bbc52f9e6133fdcbf05ed8b812b8eb, reversing
    changes made to da4ba95ca0076aa5d25f3230942d67feb1bd059f.

--- capstone-team-23/src/components/Customer_Main/CustomerDashboard.jsx
+++ capstone-team-23/src/components/Customer_Main/CustomerDashboard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { getAuth, signOut } from "firebase/auth";
 import initializeAuthentication from '../../firebase/firebase-init'
 import {  useNavigate  } from 'react-router-dom'
-import CustHeader from './CustomerHeader/Cust_Header';
 
 function CustomerDashboard() {
     
@@ -21,8 +20,6 @@ const sign_Out = () => {
   return (
     <div>CustomerDashboard
         <button className='btn btn-primary' onClick={sign_Out}>Sign Out</button>
-
-        <CustHeader></CustHeader>
     </div>
   )
 }
--- capstone-team-23/src/components/Customer_Main/CustomerHeader/Cust_Header.jsx
+++
@@ -1,9 +0,0 @@
-import Header from "../../Reusable Components/Header/Header";
-
-function CustHeader(){
-    return (<div>
-            <CustHeader></CustHeader>
-        </div>)
-}
-
-export default CustHeader;
