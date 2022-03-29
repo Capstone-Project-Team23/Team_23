b15b618bb2d750e7217e7206fae95b960bd552a6
parthppc <contracp@sheridancollege.ca>
2022-03-29 12:22:43 -0400

    search bar added

--- capstone-team-23/src/components/Customer_Main/CustomerDashboard.jsx
+++ capstone-team-23/src/components/Customer_Main/CustomerDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { getAuth, signOut } from "firebase/auth";
 import initializeAuthentication from '../../firebase/firebase-init'
 import {  useNavigate  } from 'react-router-dom'
+import SearchBar from '../Reusable Components/SearchBar/SearchBar';
 
 function CustomerDashboard() {
     
@@ -20,6 +21,7 @@ const sign_Out = () => {
   return (
     <div>CustomerDashboard
         <button className='btn btn-primary' onClick={sign_Out}>Sign Out</button>
+        <SearchBar></SearchBar>
     </div>
   )
 }
